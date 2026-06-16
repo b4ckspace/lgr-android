18f4c9820a89cd57b902534f53f4ca40713ecd89
andy-u <www.github.com@uhsemann.de>
2026-06-16 18:45:52 +0200

    Bump version to 1.0.0 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "de.backspace.lgr"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.9.0"
+        versionCode = 2
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
